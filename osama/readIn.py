18f5413f0785b798f63cd6af58321ceca3909ed7
ozzstrich <rahmanosama@me.com>
2018-03-12 16:38:07 +0000

    added readIn

--- osama/readIn.py
+++ osama/readIn.py
@@ -1,21 +1,28 @@
+# This will read in images as they are sent to the folder.
+
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import cv2
+import numpy as np
+
+i = 0
 
 
 class Watcher:
-    DIRECTORY_TO_WATCH = "images"
+    directory = "images"
 
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
         event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.schedule(event_handler, self.directory, recursive = True)
         self.observer.start()
+
         try:
             while True:
-                time.sleep(5)
+                time.sleep(1)
         except:
             self.observer.stop()
             print "Error"
@@ -27,17 +34,13 @@ class Handler(FileSystemEventHandler):
 
     @staticmethod
     def on_any_event(event):
+        global i
         if event.is_directory:
             return None
-
         elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            print "Received created event - %s." % event.src_path
-
-        elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
-            print "Received modified event - %s." % event.src_path
-
+            print "Image ", i, " taken!"
+            image = cv2.imread('output' + str(i) + '.png')
+            i += 1
 
 if __name__ == '__main__':
     w = Watcher()
